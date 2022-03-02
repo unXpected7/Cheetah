510937957998d135c029bdf5fd85a0337519ee25
hikaaam <andrenuryana@gmail.com>
2022-03-02 18:12:34 +0700

    create user entity

---
+++ BackEnd/src/entity/users.ts
@@ -0,0 +1,40 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  UpdateDateColumn,
+  CreateDateColumn,
+  BaseEntity,
+  OneToMany,
+} from "typeorm";
+import { Chat } from "./chats";
+
+@Entity({
+  name: "users",
+})
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    unique: true,
+  })
+  nickname: string;
+
+  @Column()
+  password: string;
+
+  @OneToMany(() => Chat, (Chat) => Chat.user)
+  chat: Chat[];
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
