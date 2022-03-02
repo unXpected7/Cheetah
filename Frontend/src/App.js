f727ff87fb96c4fdd6c32e86aa9b4a8c61b4a266
unXpected7 <ilyasyusuf002@gmail.com>
2022-03-02 13:51:12 +0700

    init

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Cheetah from './Cheetah';
 
 function App() {
   return (
     <div className="App">
-     
+      <Cheetah/>
     </div>
   );
 }
