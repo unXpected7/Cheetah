2f58a733e8d193526fd151afdfbf5545054ce5ea
hikaaam <andrenuryana@gmail.com>
2022-03-02 18:16:16 +0700

    apply middleware and router to index express

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -1,14 +1,23 @@
+import "reflect-metadata";
 import express from "express";
 import { port } from "./config";
-
+import { createConnection } from "typeorm";
+import { err404, invalidJson, logger } from "./middleware";
+import { ColorLog } from "./helpers";
+import Routers from "./controllers";
+import cors from "cors";
 const app = express();
 
-app.get("/", (_, res) => {
-  res.json({
-    message: "Hello World 🐆",
+createConnection().then(async () => {
+  app.use(express.json());
+  app.use(cors());
+  app.use(logger);
+  app.use(invalidJson);
+  app.use("/", Routers);
+  app.use("*", err404);
+  app.listen(port, () => {
+    console.log(
+      `Server running on port ${ColorLog.FgGreen}http://127.0.0.1:${port}`
+    );
   });
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port http://127.0.0.1:${port}`);
 });
