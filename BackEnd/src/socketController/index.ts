6a6b991caa4e44845d987ced066c7f94b8100fed
hikaaam <andrenuryana@gmail.com>
2022-03-03 12:40:26 +0700

    register all controller to index

---
+++ BackEnd/src/socketController/index.ts
@@ -0,0 +1,7 @@
+import onChat from "./onChat";
+import onJoin from "./onJoin";
+import onLeft from "./onLeft";
+import onWriting from "./onWriting";
+import onCancelWriting from "./onCancelWriting";
+
+export { onChat, onJoin, onLeft, onWriting, onCancelWriting };
