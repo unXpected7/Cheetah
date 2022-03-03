5392c848b4f222f6a03dba18bca368017a4a3434
hikaaam <andrenuryana@gmail.com>
2022-03-03 12:38:58 +0700

    Logger middleware change color  to magenta, green, red

--- BackEnd/src/middleware/logger/index.ts
+++ BackEnd/src/middleware/logger/index.ts
@@ -9,13 +9,13 @@ const logger = (req: Request, res: Response, next: NextFunction) => {
   let log = `⚡️[request]: ${date} :: IP ${ip} :: Accepted :: ${req.method} ${req.url}`;
   const playGroundCondition = req.url !== "/graphql";
   if (playGroundCondition) {
-    console.log(ColorLog.FgYellow, log);
+    console.log(ColorLog.FgMagenta, log);
   }
   res.on("finish", () => {
     let date = moment().format("Y-MM-DD HH:mm:ss");
     if (playGroundCondition) {
       console.log(
-        res.statusCode === 200 ? ColorLog.FgYellow : ColorLog.FgRed,
+        res.statusCode === 200 ? ColorLog.FgGreen : ColorLog.FgRed,
         `⚡️[response]: ${date} :: IP ${ip} :: ${res.statusCode} ${res.statusMessage} :: ${req.method} ${req.url}`
       );
     }
