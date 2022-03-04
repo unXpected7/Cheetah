486f8b73ff04bfc583e7087a86409d93aff61674
hikaaam <andrenuryana@gmail.com>
2022-03-04 11:35:14 +0700

    add login screen to appjs

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import Cheetah from './Cheetah';
+import "./App.css";
+import { Login } from "./Screens";
 
 function App() {
   return (
     <div className="App">
-      <Cheetah/>
+      <Login />
     </div>
   );
 }
