d57fc4f523c9c82a2ab6bc6145b8c4878cbb95c0
unXpected7 <ilyasyusuf002@gmail.com>
2022-03-06 16:38:49 +0700

    add Register Page

--- Frontend/src/Components/SignIn/index.jsx
+++ Frontend/src/Components/SignIn/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SignIn = () => {
     return(
         <div>
-            <h1>halamam login
+            <h1>halamam login 
             </h1>
         </div>
     )
--- Frontend/src/Components/SignUp/index.jsx
+++ Frontend/src/Components/SignUp/index.jsx
@@ -1,16 +1,56 @@
-import React from "react";
-import {Col, Container, Row,} from 'react-bootstrap';
 
 const SignUp = () => {
     return (
-        <div>
-            <Container>
-                <Row>
-                    <Col>cek</Col>
-                    <Col>cek</Col>
-                    <Col>cek</Col>
-                </Row>
-            </Container>
+        <div className="flex min-h-screen">
+            <div className="flex-1 flex items-center justify-center">
+                <div className="w-[50%]">
+                    <p className="mb-2 text-4xl font-semibold">Welcome to Cheetah</p>
+                    <p className="mb-6 text-grey-500">Sign Up your Email</p>
+                    
+                    <form className="flex flex-col gap-y-2">
+                        <label className="font-semibold text-sm">Email</label>
+                        <input 
+                            className="border py-1 px-2 rounded-lg" 
+                            placeholder="Please enter your email"
+                            />
+                    
+                        <label className="font-semibold text-sm">Password</label>
+                        <input 
+                            className="border py-1 px-2 rounded-lg"
+                            placeholder="Please enter your password"
+                            type="password" 
+                        />
+
+                        <label className="font-semibold text-sm"></label>
+                        <input
+                            className="border py-1 px-2 rounded-lg"
+                            placeholder="Please repeat your password"
+                            type="password"
+                        />
+
+                        <button className="bg-black hover:shadow-xl transition duration-300 text-white py-2 rounded-xl text-sm block">
+                            Sign Up
+                        </button>
+                    </form>
+
+                </div>
+            </div>
+                    
+            <div  
+                className="flex-1 bg-slate-50 relative " 
+                style={{
+                    background: 'url(/ilustrasi1.png)',
+                    backgroundSize: 'cover'
+                }}>
+                    <div className="absolute left-10 rigth-10 bottom-10 backdrop-blur-xl p-10 text-white">
+                    <p className="mb-1">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam quos quas voluptates,
+                    deserunt totam recusandae dicta harum nihil excepturi
+                    perspiciatis! Consequatur nemo est distinctio sequi ipsum consectetur eos optio minima?
+                    </p>
+                    <p className="text-xl text-right font-semibold">john done</p>
+                    </div>
+            </div>
         </div>
     )
 };
