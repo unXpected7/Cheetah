fb1029940907f00628d34c647bad7671f23810a0
unXpected7 <ilyasyusuf002@gmail.com>
2022-03-16 21:13:41 +0700

    refactoring

--- Frontend/src/Components/SignIn/index.jsx
+++ Frontend/src/Components/SignIn/index.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const SignIn = () => {
     return(
         <div>
-            <h1>halamam login 
-            </h1>
+            <h1>halamam login
+            </h1>     
         </div>
     )
 };
