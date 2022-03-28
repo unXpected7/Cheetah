f90eae58d5215944f49de264a36faa905e7c72b5
akmaltezar <akmalremake007@gmail.com>
2022-03-28 10:32:17 +0700

    add screen Login & Register

---
+++ Mobile/src/styles/Register.js
@@ -0,0 +1,62 @@
+import {StyleSheet} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: wp('10%'),
+  },
+  signUp: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: hp('6%'),
+    marginVertical: hp('10%'),
+    color: '#000000',
+  },
+  text: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: hp('2%'),
+    color: '#000000',
+  },
+  input: {
+    borderBottomWidth: hp('0.2%'),
+    borderBottomColor: '#9B51E0',
+    height: hp('5%'),
+    marginBottom: hp('3%'),
+  },
+  button: {
+    backgroundColor: '#2F80ED',
+    alignSelf: 'center',
+    height: hp('8%'),
+    width: wp('80%'),
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: hp('4%'),
+    borderRadius: 12,
+    borderBottomWidth: 2,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#C5C5C5',
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontFamily: 'Montserrat-Bold',
+    fontSize: hp('2.5%'),
+  },
+  optionView: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  optionText: {
+    fontFamily: 'Montserrat-Regular',
+    fontSize: hp('2.5%'),
+  },
+  optionButton: {
+    color: '#2F80ED',
+    fontFamily: 'Montserrat-Bold',
+    fontSize: hp('2.5%'),
+  },
+});
