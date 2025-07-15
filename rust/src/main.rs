f71ff5b43f93b87f6cb8121b6d0bd8f9dd2ba438
ilyaSuperGlue <andrenuryana@gmail.com>
2025-07-15 11:42:12 +0800

    rust hello world

---
+++ rust/src/main.rs
@@ -0,0 +1,50 @@
+use axum::routing::get;
+use serde_json::Value;
+use socketioxide::{
+    SocketIo,
+    extract::{AckSender, Data, SocketRef},
+};
+use tower::ServiceBuilder;
+use tower_http::cors::CorsLayer;
+use tracing::info;
+use tracing_subscriber::FmtSubscriber;
+
+fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
+    info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
+    socket.emit("auth", &data).ok();
+
+    socket.on("ping", |socket: SocketRef, Data::<Value>(data)| {
+        info!("Received event: {:?}", data);
+        socket.emit("ping", &data).ok();
+    });
+
+    socket.on("message-with-ack", |Data::<Value>(data), ack: AckSender| {
+        info!("Received event: {:?}", data);
+        ack.send(&data).ok();
+    });
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing::subscriber::set_global_default(FmtSubscriber::default())?;
+
+    let (layer, io) = SocketIo::new_layer();
+
+    let layer = ServiceBuilder::new()
+        .layer(CorsLayer::permissive()) // Enable CORS policy
+        .layer(layer);
+
+    io.ns("/", on_connect);
+    io.ns("/custom", on_connect);
+
+    let app = axum::Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(layer);
+
+    info!("Starting server");
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
